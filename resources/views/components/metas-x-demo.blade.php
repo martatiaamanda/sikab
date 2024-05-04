c627846b6e158212637b48c37eb8fc3e8759ad85
martatiaamanda <105448243+martatiaamanda@users.noreply.github.com>
2024-05-04 12:24:40 +0700

    (feat) - first

---
+++ resources/views/components/metas-x-demo.blade.php
@@ -0,0 +1,27 @@
+<!-- Nucleo Icons -->
+<link href="{{asset('assets/css/nucleo-icons.css')}}" rel="stylesheet" />
+<link href="{{asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
+
+
+<!-- Nepcha Analytics (nepcha.com) -->
+<!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+<script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+
+<!-- Fonts -->
+<link rel="dns-prefetch" href="//fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+
+<!-- Font Awesome Icons -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+<link href="{{asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
+
+<!-- Scripts -->
+{{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bppootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
+
+
+<!-- CSS Files -->
+<link id="pagestyle" href="{{asset('assets/css/soft-ui-dashboard.css?v=1.0.7')}}" rel="stylesheet" />
+<link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
---
+++ resources/views/layouts/navigator/sidebar.blade.php
@@ -0,0 +1,59 @@
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3" id="sidenav-main">
+    <div class="sidenav-header">
+      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <div class="navbar-brand m-0">
+        @if (Auth::user())
+            <img src="{{ Auth::user()->user_profile ? asset(Auth::user()->user_profile) : asset('assets/img/logo-ct-dark.png') }}" class="navbar-brand-img h-100" alt="main_logo">
+            <span class="ms-1 font-weight-bold">{{ Auth::user()->name }}</span>
+        @endif
+      </div>
+    </div>
+    <hr class="horizontal dark mt-0">
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link rounded-3  {{ Request::routeIs('home') ? 'bg-gradient-faded-info    text-white ' : '' }} " href={{ route('home') }}>
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 p-3 d-flex align-items-center justify-content-center "  ">
+                <i class="fa-solid fa-house fs-5 {{ Request::routeIs('home') ? '' : 'text-white ' }}"></i>
+            </div>
+            <span class="nav-link-text ms-1 {{ Request::routeIs('home') ? 'text-white ' : ' text-black' }}">Dashboard</span>
+          </a>
+        </li>
+
+        <li class="nav-item mt-3">
+            <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Persuratan</h6>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link rounded-3  {{ Request::routeIs('Surat.craate') ? ' bg-gradient-faded-info   text-white ' : '' }} " href={{ route('home') }}>
+              <div class="icon icon-shape icon-sm shadow border-radius-md text-center me-2 p-3 d-flex align-items-center justify-content-center bg-gradient-faded-info">
+                  <i class="fa-solid fa-house fs-5 {{ Request::routeIs('Surat.craate') ? '' : 'text-white' }}"></i>
+              </div>
+              <span class="nav-link-text ms-1 {{ Request::routeIs('Surat.craate') ? 'text-white ' : ' text-black' }}">Buat surat</span>
+            </a>
+          </li>
+
+        <li class="nav-item mt-3">
+          <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link rounded-3  {{ Request::routeIs('profile.*') ? ' active  text-white ' : '' }} " href={{ route('profile') }}>
+              <div class="icon icon-shape icon-sm shadow border-radius-md text-center me-2 p-3 d-flex align-items-center justify-content-center bg-gradient-faded-info">
+                  <i class="fa-solid fa-user fs-5 {{ Request::routeIs('profile.*') ? '' : 'text-white' }}"></i>
+              </div>
+              <span class="nav-link-text ms-1 {{ Request::routeIs('profile.*') ? 'text-white ' : ' text-black' }}">Profile</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link rounded-3" href={{ (route('logout'))}}>
+              <div class="icon icon-shape icon-sm shadow border-radius-md text-center me-2 p-3 d-flex align-items-center justify-content-center bg-gradient-faded-info ">
+                <i class="fa-solid fa-right-from-bracket fs-5 text-danger"></i>
+              </div>
+              <span class="nav-link-text ms-1 text-dark">Logout</span>
+            </a>
+          </li>
+      </ul>
+    </div>
+
+  </aside>
