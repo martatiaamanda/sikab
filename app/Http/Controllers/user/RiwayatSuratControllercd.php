24fb6ee8ec7451c573e672aa489fb202c75a57f9
martatiaamanda <105448243+martatiaamanda@users.noreply.github.com>
2024-05-07 00:59:13 +0700

    feat() - riwayat controller

--- app/Http/Controllers/user/RiwayatSuratControllercd.php
+++ app/Http/Controllers/user/RiwayatSuratControllercd.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
 use App\Models\JenisSurat;
+use App\Models\surat;
 use Illuminate\Http\Request;
 
 class RiwayatSuratControllercd extends Controller
 {
     public function index()
     {
-        $histories = JenisSurat::paginate(10);
+        $histories = surat::where('user_id', auth()->user()->id)->paginate(10);
+
+        // dd ($surats[0]->input_value);
+        // $histories = JenisSurat::paginate(10);
         return view('user.riwayat-surat', compact('histories'));
     }
 }
--- resources/views/user/riwayat-surat.blade.php
+++ resources/views/user/riwayat-surat.blade.php
@@ -12,43 +12,43 @@
                 </div>
                 <div class="card-body px-0 pt-0 pb-2">
                   <div class="table-responsive p-0">
-                    <table class="table align-items-center mb-0">
+                    <table class="table align-items-center mb-0" >
                       <thead>
                         <tr>
-                          <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Author</th>
-                          <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Function</th>
-                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
-                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Employed</th>
-                          <th class="text-secondary opacity-7"></th>
+                          <th class="text-uppercase text-info  font-weight-bolder opacity-7">NO</th>
+                          <th class="text-uppercase text-info   font-weight-bolder opacity-7 ps-2">Nomor Surat</th>
+                          <th class="text-center text-uppercase text-info  font-weight-bolder opacity-7">Jenis Surat</th>
+                          <th class="text-center text-uppercase text-info  font-weight-bolder opacity-7">Tgl Pengajuan</th>
+                          <th class="text-center text-uppercase text-info  font-weight-bolder opacity-7">Status</th>
+                          <th class="text-info opacity-7"></th>
                         </tr>
                       </thead>
                       <tbody>
                         @foreach ($histories as $history )
                         <tr>
                             <td>
-                              <div class="d-flex px-2 py-1">
-                                <div>
-                                  <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3" alt="user1">
-                                </div>
-                                <div class="d-flex flex-column justify-content-center">
-                                  <h6 class="mb-0 text-sm">John Michael</h6>
-                                  <p class="text-xs text-secondary mb-0">john@creative-tim.com</p>
-                                </div>
-                              </div>
+                                <p class=" ps-3 text-secondary  font-weight-bold">{{ $loop->iteration }}</p>
                             </td>
                             <td>
-                              <p class="text-xs font-weight-bold mb-0">Manager</p>
-                              <p class="text-xs text-secondary mb-0">Organization</p>
+                              <p class=" text-secondary  font-weight-bold">{{ $history->nomor_surat ?? 'belum ada nomornya' }}</p>
                             </td>
                             <td class="align-middle text-center text-sm">
-                              <span class="badge badge-sm bg-gradient-success">Online</span>
+                              <span class="text-secondary  font-weight-bold">{{ $history->jenis_surat->name }}</span>
                             </td>
                             <td class="align-middle text-center">
-                              <span class="text-secondary text-xs font-weight-bold">23/04/18</span>
+                              <span class="text-secondary  font-weight-bold">{{ $history->tanggal_surat}}</span>
+                            </td>
+                            <td class="align-middle text-center">
+                                <span class="badge badge-sm bg-gradient-{{ $history->status == 'diterima' ? 'success' : ($history->status == 'ditolak' ? 'danger' : 'warning')}}">{{ $history->status }}</span>
                             </td>
                             <td class="align-middle">
-                              <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                                Edit
+                                @if ($history->status != 'diterima')
+                                    <a href="javascript:;" class="text-secondary font-weight-bold text-decoration-underline pe-3" data-toggle="tooltip" data-original-title="Edit user">
+                                    Edit
+                                  </a>
+                                @endif
+                              <a href="javascript:;" class="text-secondary font-weight-bold text-decoration-underline" data-toggle="tooltip" data-original-title="Edit user">
+                                detail
                               </a>
                             </td>
                           </tr>
