88096b464cbc3a3b1fa839b751acb007e1b2f984
martatiaamanda <105448243+martatiaamanda@users.noreply.github.com>
2024-06-15 17:55:45 +0700

    fix(prod) - fix err

--- resources/views/components/layouts/sidebar-admin.blade.php
+++ resources/views/components/layouts/sidebar-admin.blade.php
@@ -36,11 +36,11 @@
         </li>
 
         <li class="nav-item">
-            <a class="nav-link rounded-3  {{ Request::routeIs('*') ? ' bg-gradient-faded-info   text-white ' : '' }} " href={{ route('admin.user') }}>
-              <div class="icon icon-shape icon-sm shadow border-radius-md text-center me-2 p-3 d-flex align-items-center justify-content-center {{ Request::routeIs('*') ? 'bg-white' : 'bg-gradient-faded-info' }}">
-                  <i class="fa-solid fa-house fs-5 text-gradient {{ Request::routeIs('*') ? 'text-info' : 'text-dark' }}"></i>
+            <a class="nav-link rounded-3  {{ Request::routeIs('admin.user*') ? ' bg-gradient-faded-info   text-white ' : '' }} " href={{ route('admin.user') }}>
+              <div class="icon icon-shape icon-sm shadow border-radius-md text-center me-2 p-3 d-flex align-items-center justify-content-center {{ Request::routeIs('admin.user*') ? 'bg-white' : 'bg-gradient-faded-info' }}">
+                  <i class="fa-solid fa-house fs-5 text-gradient {{ Request::routeIs('admin.user*') ? 'text-info' : 'text-dark' }}"></i>
               </div>
-              <span class="nav-link-text ms-1 {{ Request::routeIs('*') ? 'text-white ' : ' text-black' }}">Pengguna</span>
+              <span class="nav-link-text ms-1 {{ Request::routeIs('admin.user*') ? 'text-white ' : ' text-black' }}">Pengguna</span>
             </a>
           </li>
           {{-- belum ada list --}}
