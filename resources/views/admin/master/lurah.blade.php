b055191311ba485ceb4ca1804adc5f2f240d692c
martatiaamanda <martatiamandaa03@gmail.com>
2024-07-12 17:12:43 +0700

    fix() - prod

--- resources/views/admin/master/lurah.blade.php
+++ resources/views/admin/master/lurah.blade.php
@@ -44,7 +44,7 @@
                                             <label for="tanda_tangan">Tanda Tangan</label>
                                             <div class="">
                                                 <div class="w-25 h-25">
-                                                    <img src="{{ $lurah->tanda_tangan ? asset('/storage/lurah/' . $lurah->tanda_tangan) : 'https://via.placeholder.com/150' }}"
+                                                    <img src="{{ $lurah->tanda_tangan ? asset('' . Auth::user()->user_data->profile_ficture) : 'https://via.placeholder.com/150' }}"
                                                         alt="ttd" class="w-100 border-radius-lg shadow-sm">
                                                 </div>
                                             </div>
